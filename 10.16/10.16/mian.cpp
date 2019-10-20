b029fe351f5be138a225bc5079c18910adee28c1
joker <861585843@qq.com>
2019-10-20 23:09:12 +0800

    提交代码

---
+++ 10.16/10.16/mian.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include<windows.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = "#########";
+//	char arr2[] = "asdfghjl;";
+//	
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	while (left<=right){
+//		arr1[left] = arr2[left];
+//		arr1[right] = arr2[right];
+//		left++, right--;
+//		printf("%s\r", arr1);
+//		Sleep(1000);
+//	}
+//	system("pause");
+//
+// 
+//}
+//#include<stdio.h>
+//#include<string.h>
+//#include<Windows.h>
+//
+//#define NAME "tom"
+//#define PWD "123456"
+//
+//int main()
+//{
+//	int count = 3;
+//	while (count > 0){
+//		char name[64];
+//		printf("Please Enter Your Name# ");
+//		scanf("%s", name);
+//		char password[64];
+//		printf("please Enter Your Password# ");
+//		scanf("%s", password);
+//		if (strcmp(name, NAME) == 0 && 0 == strcmp(password, PWD))
+//		{
+//			printf("Login Success...Welcome!\n");
+//			break;
+//		}
+//		else{
+//			printf("账号密码错误！请重新尝试，你还有%d次机会\n",--count);
+//		}
+//	}
+//	if (count > 0){
+//
+//
+//		int i = 9;
+//		while (i >= 0){
+//			printf("正在为你加载:%d\r", i);
+//			Sleep(1000);
+//			i--;
+//		}
+//	}
+//	else{
+//		printf("你已经尝试多次，目前已经被锁定!\n");
+//		int i = 60;
+//		while (i >= 0){
+//			printf("离下次登陆还有%dS\r",i);
+//			Sleep(1000);
+//			i--;
+//		}
+//		
+//	}
+//	printf("\n");
+//	}
+	
